15a2a420121d2cb97a9ecf825244d491d8d986ba
Alkesh Jethava <alkeshjethava@gmail.com>
2020-04-25 15:04:28 +0530

    Updates

---
+++ precache-manifest.b2d8cbc121065ac61c0f33d92eba1970.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "022936ca9767ee54827195c37075907a",
+    "url": "/ganeshgarment/index.html"
+  },
+  {
+    "revision": "e958f1ba74df9a92ff0a",
+    "url": "/ganeshgarment/static/css/main.aab08b6d.chunk.css"
+  },
+  {
+    "revision": "72a21e9881f0bd9f6136",
+    "url": "/ganeshgarment/static/js/2.d8dd74ed.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ganeshgarment/static/js/2.d8dd74ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e958f1ba74df9a92ff0a",
+    "url": "/ganeshgarment/static/js/main.03dcf939.chunk.js"
+  },
+  {
+    "revision": "94dcf610a1e9be64bc7f",
+    "url": "/ganeshgarment/static/js/runtime-main.e624d3d7.js"
+  }
+]);
